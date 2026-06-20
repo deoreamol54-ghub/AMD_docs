42d1c48e568e9a531fd29f820e8c445aed2beb1b
Ubuntu <ubuntu@ip-172-31-38-157.ap-south-1.compute.internal>
2026-06-20 15:04:37 +0000

    AWS ec2 Full_stack_app committed

--- Full_stack_app/backend/app.py
+++ Full_stack_app/backend/app.py
@@ -2,15 +2,23 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Flask Backend Running"
+
 @app.route("/process", methods=["POST"])
 def process():
+    data = request.get_json(silent=True)
 
-    name = request.form.get("name")
-    email = request.form.get("email")
+    if data:
+        name = data.get("name")
+        email = data.get("email")
+    else:
+        name = request.form.get("name")
+        email = request.form.get("email")
 
     return f"""
     <h2>Data Received</h2>
-
     Name: {name}<br>
     Email: {email}
     """
