6748dc69dc1309db4fb1cbe6b33834722ac867dc
deoreamol54-ghub <deoreamol54@gmail.com>
2026-06-22 00:20:56 +0530

    commit terraform app

---
+++ tf_app1/backend/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Flask Backend Running"
+
+@app.route("/process", methods=["POST"])
+def process():
+    data = request.get_json(silent=True)
+
+    if data:
+        name = data.get("name")
+        email = data.get("email")
+    else:
+        name = request.form.get("name")
+        email = request.form.get("email")
+
+    return f"""
+    <h2>Data Received</h2>
+    Name: {name}<br>
+    Email: {email}
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ tf_app1/terraform/main.tf
@@ -0,0 +1,62 @@
+data "aws_ami" "amazon_linux" {
+
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*x86_64"]
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+
+  name = "flask-express-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+
+  key_name = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+  user_data = file("${path.module}/userdata.sh")
+
+  tags = {
+    Name = "Flask-Express-App"
+  }
+}
