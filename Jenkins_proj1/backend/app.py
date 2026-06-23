7b28924e5f8a05b3026958784387b1b3974f82c8
deoreamol54-ghub <deoreamol54@gmail.com>
2026-06-24 00:38:30 +0530

    commit jenkins project 1

--- Jenkins_proj1/backend/app.py
+++ Jenkins_proj1/backend/app.py
@@ -4,24 +4,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask Backend Running"
+        return "Flask Backend Running"
 
 @app.route("/process", methods=["POST"])
 def process():
-    data = request.get_json(silent=True)
+                data = request.get_json(silent=True)
 
-    if data:
-        name = data.get("name")
-        email = data.get("email")
-    else:
-        name = request.form.get("name")
-        email = request.form.get("email")
+                if data:
+                     name = data.get("name")
+                     email = data.get("email")
+                else:
+                     name = request.form.get("name")
+                     email = request.form.get("email")
 
-    return f"""
-    <h2>Data Received</h2>
-    Name: {name}<br>
-    Email: {email}
-    """
-
+                return f"""
+                <h2>Data Received</h2>
+                Name: {name}<br>
+                Email: {email}
+                """
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+        app.run(host="0.0.0.0", port=5000)
--- Jenkins_proj1/frontend/server.js
+++ Jenkins_proj1/frontend/server.js
@@ -7,22 +7,23 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index");
+            res.render("index");
 });
 
 app.post("/submit", async (req, res) => {
-    try {
-        const response = await axios.post(
-            "http://backend:5000/process",
-            req.body
-        );
+            try {
+                            const response = await axios.post(
+                                                "http://localhost:5000/process",
+                                                req.body
+                                            );
 
-        res.send(response.data);
-    } catch (error) {
-        res.send("Error connecting backend");
-    }
+                            res.send(response.data);
+                        } catch (error) {
+                                        console.error(error);
+                                        res.send("Error connecting backend");
+                                    }
 });
 
 app.listen(3000, () => {
-    console.log("Frontend running on port 3000");
+            console.log("Frontend running on port 3000");
 });
