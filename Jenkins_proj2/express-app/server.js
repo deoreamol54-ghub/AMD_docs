72f53ee8d65b952e31f4a199e5a5a8c8e7717d13
deoreamol54-ghub <deoreamol54@gmail.com>
2026-06-24 23:49:42 +0530

    add express app

---
+++ Jenkins_proj2/express-app/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+            res.render("index");
+});
+
+app.post("/submit", async (req, res) => {
+            try {
+                            const response = await axios.post(
+                                                "http://localhost:5000/process",
+                                                req.body
+                                            );
+
+                            res.send(response.data);
+                        } catch (error) {
+                                        console.error(error);
+                                        res.send("Error connecting backend");
+                                    }
+});
+
+app.listen(3000, () => {
+            console.log("Frontend running on port 3000");
+});
